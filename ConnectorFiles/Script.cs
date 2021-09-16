1b9b7afca77a33b2e512c68e35e5d692f5742143
Shlega <Alex.Shlega@ottawa.ca>
2021-09-16 08:06:53 -0400

    First commiet

---
+++ ConnectorFiles/Script.cs
@@ -0,0 +1,60 @@
+public class Script : ScriptBase
+{
+    public override async Task<HttpResponseMessage> ExecuteAsync()
+    {
+        if (this.Context.OperationId == "stringtouppercase")
+        {
+            return await this.HandleToUpperCase().ConfigureAwait(false);
+        }
+        else if (this.Context.OperationId == "sortstringarray")
+        {
+            return await this.HandleSort().ConfigureAwait(false);
+        }
+        else if (this.Context.OperationId == "jsonecho")
+        {
+            return await this.HandleEcho().ConfigureAwait(false);
+        }
+
+        HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+        response.Content = CreateJsonContent($"Unknown operation ID '{this.Context.OperationId}'");
+        return response;
+    }
+
+    private async Task<HttpResponseMessage> HandleEcho()
+    {
+        HttpResponseMessage response;
+        var contentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+        response = new HttpResponseMessage(HttpStatusCode.OK);
+        response.Content = new StringContent(contentAsString);
+        return response;
+    }
+    
+    private async Task<HttpResponseMessage> HandleToUpperCase()
+    {
+        HttpResponseMessage response;
+        var contentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+        response = new HttpResponseMessage(HttpStatusCode.OK);
+        response.Content = new StringContent(contentAsString?.ToUpper());
+        return response;
+    }
+
+    private async Task<HttpResponseMessage> HandleSort()
+    {
+        HttpResponseMessage response;
+        var contentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+        var stringJArray = JArray.Parse(contentAsString);
+        var stringArray = stringJArray.ToObject<List<string>>();
+        stringArray.Sort();
+        var result = JArray.FromObject(stringArray);
+        string stringResult = result.ToString();
+        int i = stringResult.IndexOf("[");
+        stringResult = stringResult.Substring(i);
+        i = stringResult.LastIndexOf("]");
+        stringResult = stringResult.Substring(0, i+1);
+
+        response = new HttpResponseMessage(HttpStatusCode.OK);
+        response.Content = CreateJsonContent(stringResult);
+     
+        return response;
+    }
+}
